997293328a990190e00a0563b6ec19d215ba95ee
KwonSunJae <ksun4131@gmail.com>
2023-01-12 15:19:22 +0900

    [init]

--- terraform/bin/initMavlink.sh
+++ terraform/bin/initMavlink.sh
@@ -15,8 +15,7 @@ After=syslog.target
 [Service]
 Environment=MAV_IP=${MAV_IP}
 Environment=MAV_PORT=${MAV_PORT}
-Type=oneshot
-RemainAfterExit=true
+Type=simple
 WorkingDirectory=/home/mavlink-router
 ExecStart= mavlink-routerd -e ${MAV_IP}:${MAV_PORT} 127.0.0.1:${MAV_PORT}
 
